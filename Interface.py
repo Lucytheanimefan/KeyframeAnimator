cf463ddae90ebf67ebe1c30ebaaed9206f9da287
Lucy Zhang <spothorse9.lucy@gmail.com>
2016-10-15 18:32:04 -0400

    Succesfully getting files

--- Interface.py
+++ Interface.py
@@ -1,5 +1,33 @@
-import Tkinter
+import tkinter
+from tkinter.filedialog import askopenfilename
 
-main_screen = Tkinter.Tk()
+files = []
 
-main_screen.mainloop()
+
+class App:
+
+    def __init__(self, main):
+        global files
+        main.minsize(width=666, height=666)
+        create_button("Submit", submit, 2, 3, 2, 2)
+        create_button("Add file", add_file, 30, 3, 2, 2)
+
+
+def submit():
+    print("submit")
+
+
+def add_file():
+    filename = askopenfilename()
+    files.append(filename)
+    print(files)
+
+
+def create_button(text, button_action, row, col, rowspan, colspan):
+    B = tkinter.Button(root, text=text, command=button_action)
+    B.grid(row=row, column=col, columnspan=colspan, rowspan=rowspan)
+
+root = tkinter.Tk()
+app = App(root)
+
+root.mainloop()
